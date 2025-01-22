e67aef1cbcd733d2871b40d8945ba484920bb582
sandro-byte32 <75497753+sandro-byte32@users.noreply.github.com>
2025-01-22 14:30:39 +0100

    Update translations_lang.php for auto select provider

--- application/language/german/translations_lang.php
+++ application/language/german/translations_lang.php
@@ -485,4 +485,6 @@ $lang['fields'] = 'Fields';
 $lang['invalid_credentials_provided'] = 'Invalid credentials provided, please try again.';
 $lang['calendar_url'] = 'Calendar URL';
 $lang['please_select'] = 'Bitte Wählen';
+$lang['auto_select_single_provider'] = 'Einzelnen Anbieter automatisch auswählen';
+$lang['auto_select_single_provider_hint'] = 'Automatisch den Anbieter auswählen, wenn nur einer für den gewählten Service verfügbar ist.';
 // End
